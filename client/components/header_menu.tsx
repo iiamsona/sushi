afdeef3ba8e3a50dd6f2e6eafa105c6b8cbbc5e5
iiamsona <sona22hakobyan@gmail.com>
2024-07-12 16:22:23 +0400

    menu

--- client/components/header_menu.tsx
+++ client/components/header_menu.tsx
@@ -1,11 +1,25 @@
 'use client';
 import React, { useState } from 'react';
-
+import Link from 'next/link'
+import Image from 'next/image';
+import logo from "@/public/logo.svg"
 export default function HeaderMenu() {
-  
   return (
     <nav>
-      dfvbg
+      <div className='contaioner'>
+    <div className='logo'>
+    <Image src={logo} alt="logo" />
+    </div>
+    <div className='menu'>
+    <Link href="/menu">Menu</Link>
+    <Link href="/delivery">Delivery</Link>
+    <Link href="/about_us">About us</Link>
+    //input
+    //busket
+    //user
+    //languages
+    </div>
+      </div>
     </nav>
   );
 }
