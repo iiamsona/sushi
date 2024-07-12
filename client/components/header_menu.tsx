b674be08fd9ce874edaeb52a99ffed6f8df92f7b
iiamsona <sona22hakobyan@gmail.com>
2024-07-12 15:23:50 +0400

    menu

---
+++ client/components/header_menu.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React, { useState } from 'react';
+
+type SidebarProps = {
+  className?: string;
+};
+
+export default function Sidebar({ className }: SidebarProps) {
+  const [status, setStatus] = useState(false);
+
+  const handleToggle = () => {
+    setStatus(true);
+    toggle();
+    setTimeout(() => setStatus(false), 500);
+  };
+  return (
+    <nav>
+
+    </nav>
+  );
+}
