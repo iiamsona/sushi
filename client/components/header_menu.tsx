6efe4c6c75f7cc9bb09dcf095357a61086bc74ff
iiamsona <sona22hakobyan@gmail.com>
2024-07-12 23:26:44 +0400

    languages

--- client/components/header_menu.tsx
+++ client/components/header_menu.tsx
@@ -1,34 +1,66 @@
 'use client';
-import React, { useState } from 'react';
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
-import logo from "@/public/logo.svg"
-import basket from "@/public/basket.svg"
-import user from "@/public/user.svg"
-import armenian from "@/public/languages/armenian.svg"
-import russian from "@/public/languages/russian.svg"
-import english from "@/public/languages/english.svg"
+import logo from '@/public/logo.svg';
+import basket from '@/public/basket.svg';
+import user from '@/public/user.svg';
+import armenian from '@/public/languages/armenian.svg';
+import russian from '@/public/languages/russian.svg';
+import english from '@/public/languages/english.svg';
+import { DownOutlined, GlobalOutlined } from '@ant-design/icons';
+import { Button, Dropdown, message, Space, MenuProps } from 'antd';
 
+const handleMenuClick: MenuProps['onClick'] = (e) => {
+  message.info(`Click on menu item: ${e.key}`);
+};
+
+const items: MenuProps['items'] = [
+  {
+    label: 'Armenian',
+    key: '1',
+    icon: <Image src={armenian} alt="Armenian" width={20} height={20} />,
+  },
+  {
+    label: 'Russian',
+    key: '2',
+    icon: <Image src={russian} alt="Russian" width={20} height={20} />,
+  },
+  {
+    label: 'English',
+    key: '3',
+    icon: <Image src={english} alt="English" width={20} height={20} />,
+  },
+];
+
+const menuProps = {
+  items,
+  onClick: handleMenuClick,
+};
 
 export default function HeaderMenu() {
   return (
-    <nav className='flex'>
-      <div className='contaioner flex justify-between w-full'>
-    <div className='logo'>
-    <Image src={logo} alt="logo" />
-    </div>
-
-    <div className='menu flex justify-center items-center'>
-    <Link href="/menu">Menu</Link>
-    <Link href="/delivery">Delivery</Link>
-    <Link href="/about_us">About us</Link>
-    <input type="search" name="nav-search" id="" />
-    <Image src={basket} alt="basket" />
-    <Image src={user} alt="user" />
-    <Image src={armenian} alt="languages" />
-    <Image src={russian} alt="languages" />
-    <Image src={english} alt="languages" />
-    </div>
+    <nav className="flex items-center justify-center w-full">
+      <div className="flex justify-between items-center w-9/10">
+        <div className="logo">
+          <Image src={logo} alt="logo" />
+        </div>
+        <div className="menu flex justify-between items-center w-2/5">
+          <Link href="/menu">Menu</Link>
+          <Link href="/delivery">Delivery</Link>
+          <Link href="/about_us">About us</Link>
+          <input type="search" name="nav-search" id="nav-search" placeholder="Search..." />
+          <Image src={basket} alt="basket" />
+          <Image src={user} alt="user" />
+          <Dropdown menu={menuProps}>
+            <Button>
+              <Space>
+                <GlobalOutlined />
+                <DownOutlined />
+              </Space>
+            </Button>
+          </Dropdown>
+        </div>
       </div>
     </nav>
   );
