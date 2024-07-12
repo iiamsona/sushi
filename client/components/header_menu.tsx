a02fd8d37ed8e11b94722bc4d2169a29156c75eb
Avo Partevyan <avopartevyan@MacBook-Air-Avo.local>
2024-07-13 00:56:50 +0400

    Ваше сообщение о коммите

--- client/components/header_menu.tsx
+++ client/components/header_menu.tsx
@@ -13,18 +13,18 @@ import english from "@/public/languages/english.svg"
 export default function HeaderMenu() {
   return (
     <nav className='flex'>
-      <div className='contaioner flex justify-between w-full'>
+      <div className='container flex justify-between w-full'>
     <div className='logo'>
     <Image src={logo} alt="logo" />
     </div>
 
     <div className='menu flex justify-center items-center'>
     <Link href="/menu">Menu</Link>
-    <Link href="/delivery">Delivery</Link>
-    <Link href="/about_us">About us</Link>
+    <Link  href="/delivery">Delivery</Link>
+    <Link  href="/about_us">About us</Link>
     <input type="search" name="nav-search" id="" />
     <Image src={basket} alt="basket" />
-    <Image src={user} alt="user" />
+    <Image  src={user} alt="user" />
     <Image src={armenian} alt="languages" />
     <Image src={russian} alt="languages" />
     <Image src={english} alt="languages" />
