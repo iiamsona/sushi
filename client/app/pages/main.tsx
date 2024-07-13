27e4c237cee4335080f7936eb412a619fe58fccd
iiamsona <sona22hakobyan@gmail.com>
2024-07-13 21:35:50 +0400

    main start

---
+++ client/app/pages/main.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Main() {
+  return (
+    <div>
+        main
+    </div>
+  );
+}
