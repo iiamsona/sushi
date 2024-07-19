79e65039311954a8ebe3c420a415d89d3aab7734
iiamsona <sona22hakobyan@gmail.com>
2024-07-19 22:55:00 +0400

    fix: footer position

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,9 +3,8 @@ import HeaderMenu from "@/components/header_menu";
 import Footer from "@/components/footer";
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-full flex flex-col justify-between items-center">
       <HeaderMenu/>
-      
       <Footer/>
     </div>
     
